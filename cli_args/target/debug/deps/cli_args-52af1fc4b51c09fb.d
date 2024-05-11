696730e507d99b443e5850ff2067d7d016904c50
Lizhawai <owenmawira@gmail.com>
2024-05-11 22:21:23 +0300

    Math Operations

---
+++ cli_args/target/debug/deps/cli_args-52af1fc4b51c09fb.d
@@ -0,0 +1,5 @@
+/home/ishowratio/rust_class/cli_args/target/debug/deps/libcli_args-52af1fc4b51c09fb.rmeta: src/main.rs
+
+/home/ishowratio/rust_class/cli_args/target/debug/deps/cli_args-52af1fc4b51c09fb.d: src/main.rs
+
+src/main.rs:
---
+++ cli_args/target/debug/deps/cli_args-aee018cc9a16992f.d
@@ -0,0 +1,5 @@
+/home/ishowratio/rust_class/cli_args/target/debug/deps/libcli_args-aee018cc9a16992f.rmeta: src/main.rs
+
+/home/ishowratio/rust_class/cli_args/target/debug/deps/cli_args-aee018cc9a16992f.d: src/main.rs
+
+src/main.rs:

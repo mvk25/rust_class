625b6bae736734c832e06b24d95deb961979c5d8
Lizhawai <owenmawira@gmail.com>
2024-05-11 22:10:47 +0300

    Loops in C

---
+++ loops/target/debug/deps/loops-623add0588efbdad.d
@@ -0,0 +1,5 @@
+/home/ishowratio/rust_class/loops/target/debug/deps/libloops-623add0588efbdad.rmeta: src/main.rs
+
+/home/ishowratio/rust_class/loops/target/debug/deps/loops-623add0588efbdad.d: src/main.rs
+
+src/main.rs:
---
+++ loops/target/debug/deps/loops-b2f17b6067484fe2.d
@@ -0,0 +1,5 @@
+/home/ishowratio/rust_class/loops/target/debug/deps/loops-b2f17b6067484fe2: src/main.rs
+
+/home/ishowratio/rust_class/loops/target/debug/deps/loops-b2f17b6067484fe2.d: src/main.rs
+
+src/main.rs:
